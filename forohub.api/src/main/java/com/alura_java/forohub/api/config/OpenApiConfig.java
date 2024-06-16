17d9d8306c6c50d44e1e9f11895721f7c395f96a
Omar García López <omar.garcia@omargl.net>
2024-06-16 14:06:20 -0600

    add swagger

---
+++ forohub.api/src/main/java/com/alura_java/forohub/api/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.alura_java.forohub.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ForoHub API")
+                        .version("1.0")
+                        .description("ForoHub API Documentation"));
+    }
+}
